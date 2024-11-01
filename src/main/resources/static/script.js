3f347c22e8e063baf470de8d4e8610e9b2225385
PiyushZagade <piyushzagade225@gmail.com>
2024-11-01 18:47:03 +0530

    resposive changes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -40,8 +40,8 @@ function toggleMenu() {
         $(".searchmsg").hide();
         $(".Availsearchmsg").hide();
     } else {
-        // let url = `https://groceryexpress-ynxu.onrender.com/search/${query}`;
-        let url = `http://localhost:8181/search/${query}`;
+        let url = `https://groceryexpress-ynxu.onrender.com/search/${query}`;
+        // let url = `http://localhost:8181/search/${query}`;
         fetch(url)
             .then((response) => {
                 if (!response.ok) {
